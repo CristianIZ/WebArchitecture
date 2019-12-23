f382f62a362a9a99caef0755038f4bc1d10b2b61
Cristian <cris1408@gmail.com>
2019-12-23 16:48:55 -0300

    - Se agrega el type resolver para los pedidos html en el hosts - Se cambia el web api config

--- WebProject/WebProject.Hosts.Web/App_Start/WebApiConfig.cs
+++ WebProject/WebProject.Hosts.Web/App_Start/WebApiConfig.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Web.Http;
+using System.Web.Http.Dispatcher;
+using WebProject.Hosts.Web.App_Start;
 
 namespace WebProject.Hosts.Web
 {
@@ -9,16 +12,31 @@ namespace WebProject.Hosts.Web
     {
         public static void Register(HttpConfiguration config)
         {
-            // Configuración y servicios de API web
+            var suffix = typeof(DefaultHttpControllerSelector).GetField("ControllerSuffix", BindingFlags.Static | BindingFlags.Public);
+            if (suffix != null) suffix.SetValue(null, string.Empty);
 
-            // Rutas de API web
+            config.Services.Replace(typeof(IHttpControllerTypeResolver),
+                new HttpServiceTypeResolver());
+
             config.MapHttpAttributeRoutes();
-
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
+            var appXmlType = config.Formatters.XmlFormatter.SupportedMediaTypes.FirstOrDefault(t => t.MediaType == "application/xml");
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Remove(appXmlType);
         }
+
+        #region codigo viejo
+        //public static void Register(HttpConfiguration config)
+        //{
+        //    // Configuración y servicios de API web
+
+        //    // Rutas de API web
+        //    config.MapHttpAttributeRoutes();
+
+        //    config.Routes.MapHttpRoute(
+        //        name: "DefaultApi",
+        //        routeTemplate: "api/{controller}/{id}",
+        //        defaults: new { id = RouteParameter.Optional }
+        //    );
+        //}
+        #endregion
     }
 }
